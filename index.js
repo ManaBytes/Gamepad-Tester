8b791e1b5db16439fe649db1c305ce27ebce70a1
ManaBytes <ManaBytes@proton.me>
2024-08-23 15:20:11 -0700

    Add log for controller info, add controllerIndex for pre button array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let controllerIndex = null;
+
 window.addEventListener("gamepadconnected", (event) => {
   handleConnectDisconnect(event, true);
 });
@@ -14,10 +16,15 @@ function handleConnectDisconnect(event, connected) {
     "controller-connected-area"
   );
 
+  const gamepad = event.gamepad;
+  console.log(gamepad);
+
   if (connected) {
+    controllerIndex = gamepad.index;
     controllerAreaNotConnected.style.display = "none";
     controllerAreaConnected.style.display = "block";
   } else {
+    controllerIndex = null;
     controllerAreaNotConnected.style.display = "block";
     controllerAreaConnected.style.display = "none";
   }
