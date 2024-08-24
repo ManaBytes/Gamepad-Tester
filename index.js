b0ca72b6925dcbbe52955bd6236b30cb814b8d72
ManaBytes <ManaBytes@proton.me>
2024-08-23 17:04:05 -0700

    Axed the axis bug, axes tracking fixed

--- index.js
+++ index.js
@@ -24,10 +24,23 @@ function handleConnectDisconnect(event, connected) {
     controllerAreaNotConnected.style.display = "none";
     controllerAreaConnected.style.display = "block";
     createButtonLayout(gamepad.buttons);
+    createAxesLayout(gamepad.axes);
   } else {
     controllerIndex = null;
     controllerAreaNotConnected.style.display = "block";
     controllerAreaConnected.style.display = "none";
+  }
+}
+
+function createAxesLayout(axes) {
+  const buttonsArea = document.getElementById("buttons");
+  for (let i = 0; i < axes.length; i++) {
+    buttonsArea.innerHTML += `<div id=axis-${i} class='axis'>
+                                 <div class='axis-name'>AXIS ${i}</div>
+                                 <div class='axis-value'>${axes[i].toFixed(
+                                   4
+                                 )}</div>
+                              </div> `;
   }
 }
 
@@ -95,8 +108,19 @@ function handleButtons(buttons) {
 }
 
 function handleSticks(axes) {
+  updateAxesGrid(axes);
   updateStick("controller-b10", axes[0], axes[1]);
   updateStick("controller-b11", axes[2], axes[3]);
+}
+
+function updateAxesGrid(axes) {
+  for (let i = 0; i < axes.length; i++) {
+    const axis = document.querySelector(`#axis-${i} .axis-value`);
+    const value = axes[i];
+    // if (value > 0.06 || value < -0.06) {
+    axis.innerHTML = value.toFixed(4);
+    // }
+  }
 }
 
 function updateStick(elementId, leftRightAxis, upDownAxis) {
